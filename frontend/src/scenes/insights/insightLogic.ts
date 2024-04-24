93885ce4b7966b7c0fd8d650f6e969e01c673953
Julian Bez <julian@posthog.com>
2024-04-24 20:57:46 +0200

    Make insightLogic not load things on mount when cached

--- frontend/src/scenes/insights/insightLogic.ts
+++ frontend/src/scenes/insights/insightLogic.ts
@@ -714,7 +714,7 @@ export const insightLogic = kea<insightLogicType>([
                 return
             }
 
-            if (!props.doNotLoad) {
+            if (!props.doNotLoad && !props.cachedInsight) {
                 actions.loadInsight(props.dashboardItemId as InsightShortId)
             }
         },
